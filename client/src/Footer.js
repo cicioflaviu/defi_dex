1aa87ed655f2062c4ac8991016763a6cad6e8ded
Flaviu Cicio <flaviucicio@gmail.com>
2021-03-18 21:10:48 +0200

    Initialize project

---
+++ client/src/Footer.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Footer = () => {
+  return (
+    <footer>
+      <p className="text-center">
+        <a href="https://cicio.dev" style={{color: 'white'}}>@Copyright Flaviu Cicio</a>
+      </p>
+    </footer>
+  );
+};
+
+export default Footer;
